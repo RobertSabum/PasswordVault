6781f65e7f366ac6d786cc9353712599ac3e5024
robertsabum <rjsabum@noctrl.edu>
2021-03-20 16:19:28 -0500

    Few css Changes, added some event functions

--- frontend/src/Components/Password.js
+++ frontend/src/Components/Password.js
@@ -3,15 +3,30 @@ import React, { Component } from 'react';
 class Password extends Component{
    
     render(){
+       var visible = true;
 
-       return(
+      const hideonclick = (event) => (
+         event.target.style.visibility = 'hidden',
+         event.target.style.color = 'rgba(255, 255, 255, 0)',
+         event.target.style.backgroundColor = 'rgba(148, 148, 148, 0)',
+         visible = false);
+         
+      
+      const showonclick = (event) => (
+         event.target.style.visibility = 'visible',
+         event.target.style.color = 'rgba(255, 255, 255, 1)',
+         event.target.style.backgroundColor = 'rgba(148, 148, 148, 1)',
+         visible = true);
+         
+
+      return(
          <div className = 'password'>
             <div className = 'log-in-info'>
                <p>Username: {this.props.username}</p>
                <p>Password: {this.props.password}</p>
             </div>
-            <div className = 'log-in-website'>
-               <p>{this.props.website}</p>
+            <div className = 'log-in-website' onClick = {visible? hideonclick : showonclick}>
+               {this.props.website}
             </div>
          </div>
        );
