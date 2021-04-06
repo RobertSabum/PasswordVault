37299d95c575b46e396f2d502a7bd3a5babcb4dc
robertsabum <rjsabum@noctrl.edu>
2021-04-06 16:49:48 -0500

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,99 @@
 import React, { Component } from 'react';
-import Info from './Components/Info';
-import Passwords from './Components/Passwords';
-import AddPassword from './Components/AddPassword'
-import samplepasswords from './BlockchainInteraction/SamplePasswordArray';
+import Password from './Components/Password';
+import passarray from './BlockchainInteraction/SamplePasswordArray';
 import Web3 from 'web3';
 import ChainPassAbi from './BlockchainInteraction/Abis'
-var userAddress = '';
-var userName = '';
 const contractaddress = '0xFc3Dc277617A0C54AF3B02da3Ff617975E85C889';
    
 class App extends Component{
-  
+   //contains the state of the users address, username, and stored passwords
+   state = {
+      userName: 'username',
+      userAddress: 'address',
+      passwords: []
+   }
 
-   
   render(){
+
+   //maps a password from the password array to an array of password components
+   const _passwords = this.state.passwords.map((p, i) =>
+         p[0] && <Password key={i} website={p[0]} username={p[1]} password={p[2]} />
+      );
+
+   //temporary variables to store info from multiple input fields
+   var tpass, tname, tsite;
+
+   //called when a password wants to be added
+   const addpassword = (e) => {
+      e.preventDefault(); //So the page does not refresh
+      passarray.push([tsite, tname, tpass]);//adds the temporary variables set above to the password array
+      console.log('added new password');
+      updatepasswords();//updates the state of the stored passwords
+   }
+
+   //functions to continuously update the above temporary variables whenever their respective input field is edited
+   const updatepass = (e) =>{
+      tpass = e.target.value;
+   }
+   const updatename = (e) =>{
+      tname = e.target.value;
+   }
+   const updatesite = (e) =>{
+      tsite = e.target.value;
+   }
+
+   //updates the state of the password state element
+   const updatepasswords = (e) =>{
+      this.setState(
+         (oldState) => ({
+            passwords: passarray
+         })
+      )
+      console.log('passwords updated');
+   }
+   
+   //updates the state of the users info (address and username)
+   const login = (e) => {
+      this.setState(
+         (oldState) => ({
+            userName: 'Robert',
+            userAddress: '0x00',
+         })
+      )
+      updatepasswords(e);
+      console.log('logged in');
+         
+   };
+
      return(
         <div className = 'app'>
-           <h1 id = 'appt-title'>CHAIN PASS</h1>
-           <button id = 'login-button' onClick = {this.loadWeb3}>Connect Wallet</button>
-           <Info name = {userName} address = {userAddress}/>
-           <Passwords storedpasswords = {samplepasswords}/>
-           <AddPassword />
+
+            <h1 id = 'appt-title'>CHAIN PASS</h1>
+
+            <button id = 'login-button' onClick = {login}>Connect Wallet</button>
+
+            <div id = 'info'>
+               <p>{this.state.userName}</p>
+               <p>{this.state.userAddress}</p>
+            </div>
+
+            <div id='password-accessor'>
+               <div id = 'addpasswordbox'>
+                  <form onSubmit = {null}>
+                     <input onChange = {updatename} type="text" placeholder = "Username" className = 'infoinput'/>
+               
+                     <input onChange = {updatepass} type="text" placeholder = "Password" className = 'infoinput'/>
+               
+                     <input onChange = {updatesite} type="text" placeholder = "Website" className = 'infoinput'/>
+                  </form>
+
+                  <button onClick = {addpassword} id='add-password-button'>Add Password</button>
+               </div>
+
+               <div id = 'passwords'>
+                  {_passwords}
+               </div>
+            </div>
         </div>
      );
   }
--- frontend/src/Components/Password.js
+++ frontend/src/Components/Password.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
 class Password extends Component{
-   constructor(props) {
-      super(props);
-      this.state = {showPassword: false};
-    }
    
-    render(){
+   state = {//prop storing visiblity of the password
+      showPassword: false
+   };
+    
+   
+   render(){
+
+      //functionality to modify the visibilty state of the component
       let visibility;
       if (this.state.showPassword){
          visibility = {
@@ -21,24 +24,25 @@ class Password extends Component{
          };
       }
 
-      const togglevisibility = (event) => {
+     const togglevisibility = (event) => {//function that changes the visibility
          event.preventDefault();
          this.setState((oldState) => ({
-           showPassword: !oldState.showPassword,
-         }));
-       }
-         
-      return(
-         <div className = 'password'>
-            <div className = 'log-in-info' onClick={togglevisibility}>
-               <p>Username: {this.props.username}</p>
-               <p>Password: {this.props.password}</p>
-            </div>
-            <div className = 'log-in-website' onClick={togglevisibility} style = {visibility}>
-               {this.props.website}
-            </div>
-         </div>
-       );
-    }
-  }
+         showPassword: !oldState.showPassword,
+        }));
+      }
+        
+     return(
+        <div className = 'password'>
+           <div className = 'log-in-info' onClick={togglevisibility}>
+              <p>Username: {this.props.username}</p>
+              <p>Password: {this.props.password}</p>
+           </div>
+           
+           <div className = 'log-in-website' onClick={togglevisibility} style = {visibility}>
+              {this.props.website}
+           </div>
+        </div>
+      );
+   }
+}
   export default Password;
